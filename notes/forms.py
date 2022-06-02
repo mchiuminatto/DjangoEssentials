fad29aa6e487217400abac848864a648d33765fa
Marcello Chiuminatto <mchiuminatto@gmail.com>
2022-06-02 11:32:20 -0300

    forms styled final

--- notes/forms.py
+++ notes/forms.py
@@ -8,6 +8,13 @@ class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ('title', 'text')
+        labels = {
+            'text':'Write your thoughts here'
+        }
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control my-5'}),
+            'text': forms.Textarea(attrs={'class':'form-control mb-5'})
+        }
 
     # form functionalities
     def clean_title(self):
